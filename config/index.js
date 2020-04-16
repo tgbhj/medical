1f28281ef79d1678e30ae4a00c5843f6ee4086b7
tgbhj <j1060381781@qq.com>
2020-04-16 13:51:26 +0800

    commit

--- config/index.js
+++ config/index.js
@@ -1,12 +1,9 @@
 module.exports = {
     httpPort: process.env.httpPort || 9000,
-
     mongoHost: process.env.mongoHost || '127.0.0.1',
     mongoPort: process.env.mongoPort || 27017,
     mongoDataBase: process.env.mongoDataBase || 'adinno',
-
     // mongoHost: process.env.mongoHost || '192.168.1.112',
     // mongoUser: process.env.mongoUser || 'smart',
-    // mongoPass: process.env.mongoPass || 'smart123',
-
+    // mongoPass: process.env.mongoPass || 'smart123'
 }
